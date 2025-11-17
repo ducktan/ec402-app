ad8052f3593539ec51ab14ef3e7a3032c9219cb6
Your Name <your_email@example.com>
2025-11-17 09:01:45 +0700

    m

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const userRoutes = require("./routes/user.routes");
 const productRoutes = require("./routes/product.routes");
 const CategoryRoutes = require("./routes/category.routes");
 const BrandRoutes = require("./routes/brand.routes");
+const wishlistRoutes = require("./routes/wishlist.routes");
 const path = require("path");
 
 
@@ -19,6 +20,7 @@ app.use("/api/users", userRoutes);      // thêm route user
 app.use("/api/categories", CategoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/brands", BrandRoutes);
+app.use("/api/wishlist", wishlistRoutes);
 
 
 module.exports = app;
