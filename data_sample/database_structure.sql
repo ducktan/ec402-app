7ad8d7b917fe20aff7d108e4c1168bf892f4a121
ducktan <22521303@gm.uit.edu.vn>
2025-11-17 15:59:38 +0700

    update db

--- data_sample/database_structure.sql
+++ data_sample/database_structure.sql
@@ -171,6 +171,21 @@ CREATE TABLE token_blacklist (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Wishlist
+CREATE TABLE wishlist (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    product_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_wishlist_user 
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+
+    CONSTRAINT fk_wishlist_product 
+        FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+
+    UNIQUE KEY unique_wishlist (user_id, product_id)
+);
 
 
 
