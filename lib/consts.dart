b690ac39279ea62c223e68b944b7458185aaea48
ducktan <22521303@gm.uit.edu.vn>
2025-12-07 21:35:08 +0700

    feat: checkout, voucher, payment

--- lib/consts.dart
+++
@@ -1,2 +0,0 @@
-const String stripePublishableKey = "pk_test_51Sbhsp3Gd6v4W76iBKFlsl1e6FTUEkpIl4iG8UI7qMGLgbT4HYyChpUF6NGxhFFiddP3ngTFpVLMYT6QQvzq3TdZ00cZWk6h0D"; 
-const String stripeSecretKey = "sk_test_51Sbhsp3Gd6v4W76iqrC0QAaL0SKCYEpfh0qk2E2L2Wdvkp9bt2jR0midtqRMJ4bVKwH91WkyPVcSBrHVNJShNxez005PnQqth3";
